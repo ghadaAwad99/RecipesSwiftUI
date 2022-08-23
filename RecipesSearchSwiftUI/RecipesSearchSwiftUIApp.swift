dd0b53126d694c83f24dec901aa7eefbb7d26b57
Ghada Mustafa <74993384+ghadaAwad99@users.noreply.github.com>
2022-08-23 11:50:47 +0200

    add Alamofire package

---
+++ RecipesSearchSwiftUI/RecipesSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesSearchSwiftUIApp.swift
+//  RecipesSearchSwiftUI
+//
+//  Created by Hala Mustafa on 25/01/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipesSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
